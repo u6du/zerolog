18abeba337a6cb68c90ada2c0c92aed211947172
- <->
2019-07-02 22:29:02 +0800

    add

--- debug/debug.go
+++ debug/debug.go
@@ -1,4 +1,4 @@
-package info
+package debug
 
 import (
     "fmt"
@@ -10,203 +10,203 @@ import (
 )
 
 func Msg(msg string) {
-    log.Info().Out(msg)
+    log.Debug().Out(msg)
 }
 
 func Msgf(format string, v ...interface{}) {
-    log.Info().Out(fmt.Sprintf(format, v...))
+    log.Debug().Out(fmt.Sprintf(format, v...))
 }
 
 func Fields(fields map[string]interface{}) *zerolog.Event {
-    return log.Info().Fields(fields)
+    return log.Debug().Fields(fields)
 }
 
 func Dict(key string, dict *zerolog.Event) *zerolog.Event {
-    return log.Info().Dict(key, dict)
+    return log.Debug().Dict(key, dict)
 }
 
 func Array(key string, arr zerolog.LogArrayMarshaler) *zerolog.Event {
-    return log.Info().Array(key, arr)
+    return log.Debug().Array(key, arr)
 }
 
 func Object(key string, obj zerolog.LogObjectMarshaler) *zerolog.Event {
-    return log.Info().Object(key, obj)
+    return log.Debug().Object(key, obj)
 }
 
 func EmbedObject(obj zerolog.LogObjectMarshaler) *zerolog.Event {
-    return log.Info().EmbedObject(obj)
+    return log.Debug().EmbedObject(obj)
 }
 
 func Str(key, val string) *zerolog.Event {
-    return log.Info().Str(key, val)
+    return log.Debug().Str(key, val)
 }
 
 func Strs(key string, vals []string) *zerolog.Event {
-    return log.Info().Strs(key, vals)
+    return log.Debug().Strs(key, vals)
 }
 
 func Bytes(key string, val []byte) *zerolog.Event {
-    return log.Info().Bytes(key, val)
+    return log.Debug().Bytes(key, val)
 }
 
 func Hex(key string, val []byte) *zerolog.Event {
-    return log.Info().Hex(key, val)
+    return log.Debug().Hex(key, val)
 }
 
 func RawJSON(key string, b []byte) *zerolog.Event {
-    return log.Info().RawJSON(key, b)
+    return log.Debug().RawJSON(key, b)
 }
 
 func AnErr(key string, err error) *zerolog.Event {
-    return log.Info().AnErr(key, err)
+    return log.Debug().AnErr(key, err)
 }
 
 func Errs(key string, errs []error) *zerolog.Event {
-    return log.Info().Errs(key, errs)
+    return log.Debug().Errs(key, errs)
 }
 
 func Err(err error) *zerolog.Event {
-    return log.Info().Err(err)
+    return log.Debug().Err(err)
 }
 
 func Bool(key string, b bool) *zerolog.Event {
-    return log.Info().Bool(key, b)
+    return log.Debug().Bool(key, b)
 }
 
 func Bools(key string, b []bool) *zerolog.Event {
-    return log.Info().Bools(key, b)
+    return log.Debug().Bools(key, b)
 }
 
 func Int(key string, i int) *zerolog.Event {
-    return log.Info().Int(key, i)
+    return log.Debug().Int(key, i)
 }
 
 func Ints(key string, i []int) *zerolog.Event {
-    return log.Info().Ints(key, i)
+    return log.Debug().Ints(key, i)
 }
 
 func Int8(key string, i int8) *zerolog.Event {
-    return log.Info().Int8(key, i)
+    return log.Debug().Int8(key, i)
 }
 
 func Ints8(key string, i []int8) *zerolog.Event {
-    return log.Info().Ints8(key, i)
+    return log.Debug().Ints8(key, i)
 }
 
 func Int16(key string, i int16) *zerolog.Event {
-    return log.Info().Int16(key, i)
+    return log.Debug().Int16(key, i)
 }
 
 func Ints16(key string, i []int16) *zerolog.Event {
-    return log.Info().Ints16(key, i)
+    return log.Debug().Ints16(key, i)
 }
 
 func Int32(key string, i int32) *zerolog.Event {
-    return log.Info().Int32(key, i)
+    return log.Debug().Int32(key, i)
 }
 
 func Ints32(key string, i []int32) *zerolog.Event {
-    return log.Info().Ints32(key, i)
+    return log.Debug().Ints32(key, i)
 }
 
 func Int64(key string, i int64) *zerolog.Event {
-    return log.Info().Int64(key, i)
+    return log.Debug().Int64(key, i)
 }
 
 func Ints64(key string, i []int64) *zerolog.Event {
-    return log.Info().Ints64(key, i)
+    return log.Debug().Ints64(key, i)
 }
 
 func Uint(key string, i uint) *zerolog.Event {
-    return log.Info().Uint(key, i)
+    return log.Debug().Uint(key, i)
 }
 
 func Uints(key string, i []uint) *zerolog.Event {
-    return log.Info().Uints(key, i)
+    return log.Debug().Uints(key, i)
 }
 
 func Uint8(key string, i uint8) *zerolog.Event {
-    return log.Info().Uint8(key, i)
+    return log.Debug().Uint8(key, i)
 }
 
 func Uints8(key string, i []uint8) *zerolog.Event {
-    return log.Info().Uints8(key, i)
+    return log.Debug().Uints8(key, i)
 }
 
 func Uint16(key string, i uint16) *zerolog.Event {
-    return log.Info().Uint16(key, i)
+    return log.Debug().Uint16(key, i)
 }
 
 func Uints16(key string, i []uint16) *zerolog.Event {
-    return log.Info().Uints16(key, i)
+    return log.Debug().Uints16(key, i)
 }
 
 func Uint32(key string, i uint32) *zerolog.Event {
-    return log.Info().Uint32(key, i)
+    return log.Debug().Uint32(key, i)
 }
 
 func Uints32(key string, i []uint32) *zerolog.Event {
-    return log.Info().Uints32(key, i)
+    return log.Debug().Uints32(key, i)
 }
 
 func Uint64(key string, i uint64) *zerolog.Event {
-    return log.Info().Uint64(key, i)
+    return log.Debug().Uint64(key, i)
 }
 
 func Uints64(key string, i []uint64) *zerolog.Event {
-    return log.Info().Uints64(key, i)
+    return log.Debug().Uints64(key, i)
 }
 
 func Float32(key string, f float32) *zerolog.Event {
-    return log.Info().Float32(key, f)
+    return log.Debug().Float32(key, f)
 }
 
 func Floats32(key string, f []float32) *zerolog.Event {
-    return log.Info().Floats32(key, f)
+    return log.Debug().Floats32(key, f)
 }
 
 func Float64(key string, f float64) *zerolog.Event {
-    return log.Info().Float64(key, f)
+    return log.Debug().Float64(key, f)
 }
 
 func Floats64(key string, f []float64) *zerolog.Event {
-    return log.Info().Floats64(key, f)
+    return log.Debug().Floats64(key, f)
 }
 
 func Time(key string, t time.Time) *zerolog.Event {
-    return log.Info().Time(key, t)
+    return log.Debug().Time(key, t)
 }
 
 func Times(key string, t []time.Time) *zerolog.Event {
-    return log.Info().Times(key, t)
+    return log.Debug().Times(key, t)
 }
 
 func Dur(key string, d time.Duration) *zerolog.Event {
-    return log.Info().Dur(key, d)
+    return log.Debug().Dur(key, d)
 }
 
 func Durs(key string, d []time.Duration) *zerolog.Event {
-    return log.Info().Durs(key, d)
+    return log.Debug().Durs(key, d)
 }
 
 func TimeDiff(key string, t time.Time, start time.Time) *zerolog.Event {
-    return log.Info().TimeDiff(key, t, start)
+    return log.Debug().TimeDiff(key, t, start)
 }
 
 func Interface(key string, i interface{}) *zerolog.Event {
-    return log.Info().Interface(key, i)
+    return log.Debug().Interface(key, i)
 }
 
 func IPAddr(key string, ip net.IP) *zerolog.Event {
-    return log.Info().IPAddr(key, ip)
+    return log.Debug().IPAddr(key, ip)
 }
 
 func IPPrefix(key string, pfx net.IPNet) *zerolog.Event {
-    return log.Info().IPPrefix(key, pfx)
+    return log.Debug().IPPrefix(key, pfx)
 }
 
 func MACAddr(key string, ha net.HardwareAddr) *zerolog.Event {
-    return log.Info().MACAddr(key, ha)
+    return log.Debug().MACAddr(key, ha)
 }
 
 
--- warn/warn.go
+++ warn/warn.go
@@ -1,4 +1,4 @@
-package info
+package warn
 
 import (
     "fmt"
@@ -10,203 +10,203 @@ import (
 )
 
 func Msg(msg string) {
-    log.Info().Out(msg)
+    log.Warn().Out(msg)
 }
 
 func Msgf(format string, v ...interface{}) {
-    log.Info().Out(fmt.Sprintf(format, v...))
+    log.Warn().Out(fmt.Sprintf(format, v...))
 }
 
 func Fields(fields map[string]interface{}) *zerolog.Event {
-    return log.Info().Fields(fields)
+    return log.Warn().Fields(fields)
 }
 
 func Dict(key string, dict *zerolog.Event) *zerolog.Event {
-    return log.Info().Dict(key, dict)
+    return log.Warn().Dict(key, dict)
 }
 
 func Array(key string, arr zerolog.LogArrayMarshaler) *zerolog.Event {
-    return log.Info().Array(key, arr)
+    return log.Warn().Array(key, arr)
 }
 
 func Object(key string, obj zerolog.LogObjectMarshaler) *zerolog.Event {
-    return log.Info().Object(key, obj)
+    return log.Warn().Object(key, obj)
 }
 
 func EmbedObject(obj zerolog.LogObjectMarshaler) *zerolog.Event {
-    return log.Info().EmbedObject(obj)
+    return log.Warn().EmbedObject(obj)
 }
 
 func Str(key, val string) *zerolog.Event {
-    return log.Info().Str(key, val)
+    return log.Warn().Str(key, val)
 }
 
 func Strs(key string, vals []string) *zerolog.Event {
-    return log.Info().Strs(key, vals)
+    return log.Warn().Strs(key, vals)
 }
 
 func Bytes(key string, val []byte) *zerolog.Event {
-    return log.Info().Bytes(key, val)
+    return log.Warn().Bytes(key, val)
 }
 
 func Hex(key string, val []byte) *zerolog.Event {
-    return log.Info().Hex(key, val)
+    return log.Warn().Hex(key, val)
 }
 
 func RawJSON(key string, b []byte) *zerolog.Event {
-    return log.Info().RawJSON(key, b)
+    return log.Warn().RawJSON(key, b)
 }
 
 func AnErr(key string, err error) *zerolog.Event {
-    return log.Info().AnErr(key, err)
+    return log.Warn().AnErr(key, err)
 }
 
 func Errs(key string, errs []error) *zerolog.Event {
-    return log.Info().Errs(key, errs)
+    return log.Warn().Errs(key, errs)
 }
 
 func Err(err error) *zerolog.Event {
-    return log.Info().Err(err)
+    return log.Warn().Err(err)
 }
 
 func Bool(key string, b bool) *zerolog.Event {
-    return log.Info().Bool(key, b)
+    return log.Warn().Bool(key, b)
 }
 
 func Bools(key string, b []bool) *zerolog.Event {
-    return log.Info().Bools(key, b)
+    return log.Warn().Bools(key, b)
 }
 
 func Int(key string, i int) *zerolog.Event {
-    return log.Info().Int(key, i)
+    return log.Warn().Int(key, i)
 }
 
 func Ints(key string, i []int) *zerolog.Event {
-    return log.Info().Ints(key, i)
+    return log.Warn().Ints(key, i)
 }
 
 func Int8(key string, i int8) *zerolog.Event {
-    return log.Info().Int8(key, i)
+    return log.Warn().Int8(key, i)
 }
 
 func Ints8(key string, i []int8) *zerolog.Event {
-    return log.Info().Ints8(key, i)
+    return log.Warn().Ints8(key, i)
 }
 
 func Int16(key string, i int16) *zerolog.Event {
-    return log.Info().Int16(key, i)
+    return log.Warn().Int16(key, i)
 }
 
 func Ints16(key string, i []int16) *zerolog.Event {
-    return log.Info().Ints16(key, i)
+    return log.Warn().Ints16(key, i)
 }
 
 func Int32(key string, i int32) *zerolog.Event {
-    return log.Info().Int32(key, i)
+    return log.Warn().Int32(key, i)
 }
 
 func Ints32(key string, i []int32) *zerolog.Event {
-    return log.Info().Ints32(key, i)
+    return log.Warn().Ints32(key, i)
 }
 
 func Int64(key string, i int64) *zerolog.Event {
-    return log.Info().Int64(key, i)
+    return log.Warn().Int64(key, i)
 }
 
 func Ints64(key string, i []int64) *zerolog.Event {
-    return log.Info().Ints64(key, i)
+    return log.Warn().Ints64(key, i)
 }
 
 func Uint(key string, i uint) *zerolog.Event {
-    return log.Info().Uint(key, i)
+    return log.Warn().Uint(key, i)
 }
 
 func Uints(key string, i []uint) *zerolog.Event {
-    return log.Info().Uints(key, i)
+    return log.Warn().Uints(key, i)
 }
 
 func Uint8(key string, i uint8) *zerolog.Event {
-    return log.Info().Uint8(key, i)
+    return log.Warn().Uint8(key, i)
 }
 
 func Uints8(key string, i []uint8) *zerolog.Event {
-    return log.Info().Uints8(key, i)
+    return log.Warn().Uints8(key, i)
 }
 
 func Uint16(key string, i uint16) *zerolog.Event {
-    return log.Info().Uint16(key, i)
+    return log.Warn().Uint16(key, i)
 }
 
 func Uints16(key string, i []uint16) *zerolog.Event {
-    return log.Info().Uints16(key, i)
+    return log.Warn().Uints16(key, i)
 }
 
 func Uint32(key string, i uint32) *zerolog.Event {
-    return log.Info().Uint32(key, i)
+    return log.Warn().Uint32(key, i)
 }
 
 func Uints32(key string, i []uint32) *zerolog.Event {
-    return log.Info().Uints32(key, i)
+    return log.Warn().Uints32(key, i)
 }
 
 func Uint64(key string, i uint64) *zerolog.Event {
-    return log.Info().Uint64(key, i)
+    return log.Warn().Uint64(key, i)
 }
 
 func Uints64(key string, i []uint64) *zerolog.Event {
-    return log.Info().Uints64(key, i)
+    return log.Warn().Uints64(key, i)
 }
 
 func Float32(key string, f float32) *zerolog.Event {
-    return log.Info().Float32(key, f)
+    return log.Warn().Float32(key, f)
 }
 
 func Floats32(key string, f []float32) *zerolog.Event {
-    return log.Info().Floats32(key, f)
+    return log.Warn().Floats32(key, f)
 }
 
 func Float64(key string, f float64) *zerolog.Event {
-    return log.Info().Float64(key, f)
+    return log.Warn().Float64(key, f)
 }
 
 func Floats64(key string, f []float64) *zerolog.Event {
-    return log.Info().Floats64(key, f)
+    return log.Warn().Floats64(key, f)
 }
 
 func Time(key string, t time.Time) *zerolog.Event {
-    return log.Info().Time(key, t)
+    return log.Warn().Time(key, t)
 }
 
 func Times(key string, t []time.Time) *zerolog.Event {
-    return log.Info().Times(key, t)
+    return log.Warn().Times(key, t)
 }
 
 func Dur(key string, d time.Duration) *zerolog.Event {
-    return log.Info().Dur(key, d)
+    return log.Warn().Dur(key, d)
 }
 
 func Durs(key string, d []time.Duration) *zerolog.Event {
-    return log.Info().Durs(key, d)
+    return log.Warn().Durs(key, d)
 }
 
 func TimeDiff(key string, t time.Time, start time.Time) *zerolog.Event {
-    return log.Info().TimeDiff(key, t, start)
+    return log.Warn().TimeDiff(key, t, start)
 }
 
 func Interface(key string, i interface{}) *zerolog.Event {
-    return log.Info().Interface(key, i)
+    return log.Warn().Interface(key, i)
 }
 
 func IPAddr(key string, ip net.IP) *zerolog.Event {
-    return log.Info().IPAddr(key, ip)
+    return log.Warn().IPAddr(key, ip)
 }
 
 func IPPrefix(key string, pfx net.IPNet) *zerolog.Event {
-    return log.Info().IPPrefix(key, pfx)
+    return log.Warn().IPPrefix(key, pfx)
 }
 
 func MACAddr(key string, ha net.HardwareAddr) *zerolog.Event {
-    return log.Info().MACAddr(key, ha)
+    return log.Warn().MACAddr(key, ha)
 }
 
 
